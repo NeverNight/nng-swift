16fbc50d79c75c2e205be3099c39598d3d128f71
Måns Siljehav <mans@graphiclife.com>
2018-11-28 23:26:22 +0100

    options

--- Sources/NNG/Socket.swift
+++ Sources/NNG/Socket.swift
@@ -1,6 +1,36 @@
 import Cnng
 
 class Socket {
+  enum Option: String {
+    enum OptionValue {
+      case int(Int)
+      case ms(Int32)
+      case string(String)
+      case size(Int)
+      case uint64(UInt64)
+    }
+
+    case socketName = "socket-name"
+    case raw = "raw"
+    case `protocol` = "protocol"
+    case protocolName = "protocol-name"
+    case peer = "peer"
+    case peerName = "peer-name"
+    case recvBuffer = "recv-buffer"
+    case sendBuffer = "send-buffer"
+    case recvFd = "recv-fd"
+    case sendFd = "send-fd"
+    case recvTimeout = "recv-timeout"
+    case sendTimeout  = "send-timeout"
+    case localAddress = "local-address"
+    case remoteAddress = "remote-address"
+    case url = "url"
+    case ttlMax = "ttl-max"
+    case rectSizeMax = "recv-size-max"
+    case reconnectTimeMin = "reconnect-time-min"
+    case reconnectTimeMax = "reconnect-time-max"
+  }
+
   var socket: nng_socket
 
   init(socket: nng_socket) {
@@ -8,7 +38,6 @@ class Socket {
   }
 
   deinit {
-    print("socket deinit")
     nng_close(socket)
   }
 
@@ -26,5 +55,13 @@ class Socket {
     }
 
     return true
+  }
+
+  func getopt(_ option: Option) -> OptionValue {
+
+  }
+
+  func getopt(_ option: Option) -> OptionValue {
+
   }
 }
